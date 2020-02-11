0d12067f91d9869613b510d4f2133efd118e22df
bella-le <le.bella98@gmail.com>
2020-02-11 12:45:26 -0800

    AJAX and FB lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,6 +25,17 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
+	$.get('/project/' + idNumber, addProject);
 	console.log("User clicked on project " + idNumber);
+}
+
+function addProject(result) {
+	var projectHTML = '<a href="#" class="thumbnail">' +
+	  '<img src="' + result['image'] + '" class="detailsImage">' +
+	  '<p>' + result['title'] + '</p>' +
+	  '<p><small>' + result['date'] +
+	  '</small></p></a>' +
+	  '<p>' + result['summary'] + '</p>';
+
+	$('#project' + result.id).find('.thumbnail').find('.details').html(projectHTML);
 }
